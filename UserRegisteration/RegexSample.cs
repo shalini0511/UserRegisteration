4d220ef95b6da977303079b6ce4b22afce28696b
shalini <shalini.v@tvsnext.io>
2021-07-14 15:02:33 +0530

    [shalini]Atleast one digit

--- UserRegisteration/RegexSample.cs
+++ UserRegisteration/RegexSample.cs
@@ -103,7 +103,7 @@ namespace UserRegisteration
         public void ValidatingPassWord()
         {
             //string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
-            string passwordPattern = @"^(?=.*[A-Z]).{8,}$";
+            string passwordPattern = @"^[0-9]+[\s]+[0-9]{10}$";
             Regex regex = new Regex(passwordPattern);
             Console.WriteLine("Enter password minimum 8 characters with one upper case");
             string password = Console.ReadLine();
