a9fa2c74287a0d93f67c3a317247e24e7b1742e8
shalini <shalini.v@tvsnext.io>
2021-07-14 15:12:14 +0530

    [shalini]One Special Character

--- UserRegisteration/RegexSample.cs
+++ UserRegisteration/RegexSample.cs
@@ -103,7 +103,7 @@ namespace UserRegisteration
         public void ValidatingPassWord()
         {
             //string passwordPattern = @"[a-z,A-Z,0-9]{8,}$";
-            string passwordPattern = @"^[0-9]+[\s]+[0-9]{10}$";
+            string passwordPattern = @"(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*[<>`])(?=[^.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\]*[.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\][^.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\]*$).{8}$";
             Regex regex = new Regex(passwordPattern);
             Console.WriteLine("Enter password minimum 8 characters with one upper case");
             string password = Console.ReadLine();
