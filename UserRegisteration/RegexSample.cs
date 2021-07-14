1d28d488464896f4847fe8b55e432b806353731b
shalini <shalini.v@tvsnext.io>
2021-07-14 13:18:27 +0530

    [shalini]valid first name

---
+++ UserRegisteration/RegexSample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegisteration
+{
+    class RegexSample
+    {
+        string pattern = "^[A-Za-z]{2,}$";
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Validating the first name");
+            ValidatingFirstName();
+
+        }
+
+       
+
+        public void ValidatingFirstName()
+        {
+            Regex Regex = new Regex(pattern);
+            Console.WriteLine("Enter the first name:");
+            string input = Console.ReadLine();
+            bool res = Regex.IsMatch(input);
+            if (res)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
